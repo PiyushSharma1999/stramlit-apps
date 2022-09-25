4627fb4c092ee410a384163138f87c0f7b09f6f6
Piyush Sharma <piyush316692@gmail.com>
2022-09-25 21:03:49 +0530

    Added natophonetics and letter converter functions

--- passwordgen-app/app.py
+++ passwordgen-app/app.py
@@ -26,3 +26,20 @@ def generate_random_password2(characters:list,password_length=6)->str:
     password = [random.choice(characters) for i in range(password_length)]
     result = "".join(password)
     return result
+
+natophonetics = {"A":"Alpha","B":"Bravo","C":"Charlie","D":"Delta","E":"Echo","F":"Foxtrot","G":"Golf","H":"Hotel","I":"India","J":"Juliett","K":"Kilo","L":"Lima","M":"Mike","N":"November","O":"Oscar","P":"Papa","Q":"Quebec","R":"Romeo","S":"Sierra","T":"Tango","U":"Uniform","V":"Victor","W":"Whiskey","X":"X-Ray","Y":"Yankee","Z":"Zulu"}
+leet_dict = {'a': '4','b': 'l3', 'c': '(', 'd': '[)', 'e': '3', 'l': '1', 's': '5', 't': '+', 'w': '\\/\\/', 'o': '0'}
+
+def get_value(val, my_dict):
+    for key, value in my_dict.items():
+        if val == key:
+            return value
+
+def get_natophonetics(term):
+    result = " ".join([natophonetics.get(i,i) for i in list(term.upper())])
+    return result
+
+def leet_converter(term):
+    result = " ".join([leet_dict.get(i,i) for i in list(term.lower())])
+    return result
+    
