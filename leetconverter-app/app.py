38e43843be29602664907bee34404470dc838e4c
Piyush Sharma <piyush316692@gmail.com>
2022-09-25 20:12:29 +0530

    Added plotting function to leetconverter-app

--- leetconverter-app/app.py
+++ leetconverter-app/app.py
@@ -1,6 +1,26 @@
 import streamlit as st
 
-from worklcloud import Workcloud
+from wordcloud import WordCloud
 
 import matplotlib.pyplot as plt
 
+import matplotlib
+
+matplotlib.use('Agg')
+
+# Data
+
+natophonetics = {"A":"Alpha","B":"Bravo","C":"Charlie","D":"Delta","E":"Echo","F":"Foxtrot","G":"Golf","H":"Hotel","I":"India","J":"Juliett","K":"Kilo","L":"Lima","M":"Mike","N":"November","O":"Oscar","P":"Papa","Q":"Quebec","R":"Romeo","S":"Sierra","T":"Tango","U":"Uniform","V":"Victor","W":"Whiskey","X":"X-Ray","Y":"Yankee","Z":"Zulu"}
+leet_dict = {'a': '4','b': 'l3', 'c': '(', 'd': '[)', 'e': '3', 'l': '1', 's': '5', 't': '+', 'w': '\\/\\/', 'o': '0'}
+leet_dict_gk = {'a': 'α', 'b': 'β', 'g': 'γ', 'd': 'δ', 'e': 'ε', 'z': 'ζ', 'h': 'η', 'th': 'θ', 'i': 'ι', 'k': 'κ', 'l': 'λ', 'm': 'μ', 'n': 'ν', 'x': 'ξ', 'o': 'ω', 'p': 'π', 'r': 'ρ', 't': 'τ', 'u': 'υ', 'ph': 'φ', 'ch': 'χ', 'ps': 'ψ', 's': 'σ'}
+
+
+# Utils Functions
+
+def plot_wordcloud(docx):
+    mywordcloud = WordCloud().generate(docx)
+    fig = plt.figure(figsize=(20,10))
+    plt.imshow(mywordcloud, interpolation='bilinear')
+    plt.axis('off')
+    st.pyplot(fig)
+    
