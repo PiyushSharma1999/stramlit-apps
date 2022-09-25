843298f3db6863db160730044f5fcaa22b39ecf0
Piyush Sharma <piyush316692@gmail.com>
2022-09-25 20:49:07 +0530

    Added main to leetconverter-app/app.py

--- leetconverter-app/app.py
+++ leetconverter-app/app.py
@@ -1,3 +1,5 @@
+from asyncore import write
+from unittest import result
 import streamlit as st
 
 from wordcloud import WordCloud
@@ -40,3 +42,55 @@ def leet_converter(term):
 def greek_leet_converter(term):
     result = ' '.join([leet_dict_gk.get(i,i) for i in list(term.lower())])
     return result
+
+def main():
+    st.title("LeetSpeak App")
+    st.subheader("Streamlit App")
+
+    menu = ["Home","NatoPhonetics","About"]
+    choice = st.sidebar.selectbox("Menu",menu)
+
+    if choice == "Home":
+        st.subheader("Home")
+        # Layout
+        col1,col2 = st.columns(2)
+
+        with col1:
+            st.info("Leet Converter")
+            with st.form("myform", clear_on_submit=True):
+                raw_text = st.text_area("Your Text")
+                leet_choice = st.selectbox("Leet Choice",["Normal","GreekLeet"])
+                submit_button = st.form_submit_button("Convert")
+            
+            if submit_button:
+                st.success("Results")
+                st.write("Original:: {}".format(raw_text))
+                if  leet_choice == "GreekLeet":
+                    results = greek_leet_converter(raw_text)
+                else:
+                    results = leet_converter(raw_text)
+
+                st.code(results)
+        
+        with col2:
+            st.success("Nato Phonetizer")
+            with st.form("mynatoform",clear_on_submit=True):
+                raw_text = st.text_area("Your Text")
+                submit_button = st.form_submit_button("Phonetizer")
+            
+            if submit_button:
+                st.success("Results")
+                st.write("Original:: {}".format(raw_text))
+                results = get_natophonetics(raw_text)
+                st.code(results)
+
+                with st.expander("Visualize"):
+                    plot_wordcloud(results)
+    
+    else:
+        st.subheader("About")
+        st.success("Built wwith Streamlit")
+        st.text("CS Hustler")
+
+if __name__ == '__main__':
+    main()
