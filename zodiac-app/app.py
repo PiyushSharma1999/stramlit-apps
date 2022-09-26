aacb9f3b4129515293d57ef63c4c7704cf6e1483
Piyush Sharma <piyush316692@gmail.com>
2022-09-26 23:26:53 +0530

    Added load_data and main function

--- zodiac-app/app.py
+++ zodiac-app/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-monthlist = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+month_list = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
 # Get the zodiac using the month and the day
 
@@ -31,4 +31,60 @@ def find_zodiac(month,day):
 	elif month == 'november':
 		astro_sign = 'scorpio' if (day < 22) else 'sagittarius'
 	return astro_sign
+
+@st.cache
+def load_data(data):
+	return pd.read_csv(data)
     
+def main():
+	st.title("Zodiac App")
+	st.subheader("Streamlit Projects")
+
+	menu = ["Home","ZodiacBoard","About"]
+	choice = st.sidebar.selectbox("Menu",menu)
+	df = load_data('data/zodiac_data.csv')
+
+	if choice == "Home":
+		st.subheader("Home")
+		dob = st.date_input("Date of Birth")
+		month_of_birth = st.selectbox("Month",month_list)
+		day_of_birth =  st.number_input("Date",min_value=1,max_value=31)
+		if st.button("Predict"):
+			# st.write(dob)
+			results = find_zodiac(month_of_birth.lower(),day_of_birth)
+			st.success("Results")
+			st.write("You are ::{}".format(results))
+
+			zdf = df[df['horoscope'] == results.title()]
+			# st.dataframe(zdf)
+			st.write('Alias:: {}'.format(zdf.iloc[0].aliasname))
+
+			rcol1,rcol2,rcol3 = st.columns([2,1,1])
+
+			with rcol1:
+				st.info("Description")
+				st.write(zdf.iloc[0].description)
+
+			with rcol2:
+				with st.expander("Positives "):
+					st.write(zdf.iloc[0].positives.split(','))
+
+			with rcol3:
+				with st.expander("Negatives "):
+					st.write(zdf.iloc[0].negatives.split(','))
+
+
+
+
+
+	elif choice == "ZodiacBoard":
+		st.subheader("ZodiacBoard")
+		st.dataframe(df)
+
+
+	else:
+		st.subheader("About")
+
+
+if __name__ == '__main__':
+	main()
